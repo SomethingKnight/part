14a1ff125af44746b41ac2babd339ab411155177
SomethingKnight <2048558102@qq.com>
2023-07-16 23:58:13 +0800

    1.lvgl的按钮练习

--- my_gui.c
+++ my_gui.c
@@ -6,26 +6,22 @@ lv_obj_t *obj2;
 
 static void my_event_cb(lv_event_t *e)
 {
-	lv_obj_t   *target = lv_event_get_target(e); 		 /* 第一步：获取触发事件的部件 */
-	if ( target == obj1 )				 /* 第二步：判断触发事件的部件 */
+	lv_event_code_t code = lv_event_get_code(e); 		 /* 第一步：获取触发事件的部件 */
+	if ( code == LV_EVENT_VALUE_CHANGED )				 /* 第二步：判断触发事件的部件 */
 	{
         printf("父对象触发事件 \r\n");	 		 /* 第三步：执行相应操作 */
 	}
-	else if ( target == obj2 )
-	{
-        printf("子对象触发事件 \r\n");
-	}
-
-
 }
 
 void my_gui(void)
 {
-    obj1 = lv_obj_create(lv_scr_act());
-    lv_obj_add_event_cb(obj1,my_event_cb, LV_EVENT_CLICKED, NULL);
-    lv_obj_set_size(obj1,300,300);
-    //lv_obj_add_event_cb(obj1,my_event_cb, LV_EVENT_LONG_PRESSED, NULL);
+    lv_obj_t  *btn  = lv_btn_create( lv_scr_act() );
 
-    obj2 = lv_obj_create(lv_scr_act());
-    lv_obj_add_event_cb(obj2,my_event_cb, LV_EVENT_LONG_PRESSED, NULL);
+    lv_obj_set_size( btn, 100, 50 );								    /* 大小 */
+    lv_obj_set_align( btn, LV_ALIGN_CENTER );						    /* 对齐 */
+    lv_obj_set_style_bg_color( btn, lv_color_hex(0xffe1d4), LV_STATE_PRESSED );
+
+    lv_obj_add_flag( btn, LV_OBJ_FLAG_CHECKABLE );
+    lv_obj_add_event_cb( btn, my_event_cb, LV_EVENT_VALUE_CHANGED,NULL );	    /* 添加事件 */
+
 }
