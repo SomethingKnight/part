d73ecea86c3ba241cc177f960182a7eb937eb50e
SomethingKnight <2048558102@qq.com>
2023-07-16 20:36:23 +0800

    1.lvgl的事件基础组件

--- my_gui.c
+++ my_gui.c
@@ -1,12 +1,31 @@
 #include "my_gui.h"
 #include "lvgl.h"
 
+lv_obj_t *obj1;
+lv_obj_t *obj2;
+
+static void my_event_cb(lv_event_t *e)
+{
+	lv_obj_t   *target = lv_event_get_target(e); 		 /* 第一步：获取触发事件的部件 */
+	if ( target == obj1 )				 /* 第二步：判断触发事件的部件 */
+	{
+        printf("父对象触发事件 \r\n");	 		 /* 第三步：执行相应操作 */
+	}
+	else if ( target == obj2 )
+	{
+        printf("子对象触发事件 \r\n");
+	}
+
+
+}
+
 void my_gui(void)
 {
-    lv_obj_t *slider = lv_slider_create(lv_scr_act());
-    lv_obj_set_align(slider, LV_ALIGN_CENTER);
+    obj1 = lv_obj_create(lv_scr_act());
+    lv_obj_add_event_cb(obj1,my_event_cb, LV_EVENT_CLICKED, NULL);
+    lv_obj_set_size(obj1,300,300);
+    //lv_obj_add_event_cb(obj1,my_event_cb, LV_EVENT_LONG_PRESSED, NULL);
 
-    lv_obj_set_style_bg_color(slider, lv_color_hex(0x0c66ba),LV_STATE_DEFAULT|LV_PART_INDICATOR);
-    lv_obj_set_style_bg_color(slider, lv_color_hex(0x0c66ba),LV_STATE_DEFAULT|LV_PART_KNOB);
-
+    obj2 = lv_obj_create(lv_scr_act());
+    lv_obj_add_event_cb(obj2,my_event_cb, LV_EVENT_LONG_PRESSED, NULL);
 }
