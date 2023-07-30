a67b233d17320b07e99fcf9d2f025e5821ed2e46
SomethingKnight <2048558102@qq.com>
2023-07-30 16:14:20 +0800

    1.lvgl的按钮部件练习1

--- my_gui.c
+++ my_gui.c
@@ -1,27 +1,33 @@
 #include "my_gui.h"
 #include "lvgl.h"
 
-lv_obj_t *obj1;
-lv_obj_t *obj2;
+//lv_obj_t *obj1;
+//lv_obj_t *obj2;
+lv_obj_t   *switch1;
 
-static void my_event_cb(lv_event_t *e)
+static void event_cb(lv_event_t *e)
 {
 	lv_event_code_t code = lv_event_get_code(e); 		 /* 第一步：获取触发事件的部件 */
 	if ( code == LV_EVENT_VALUE_CHANGED )				 /* 第二步：判断触发事件的部件 */
 	{
-        printf("父对象触发事件 \r\n");	 		 /* 第三步：执行相应操作 */
+        if(lv_obj_has_state(switch1, LV_STATE_CHECKED))
+        {
+            printf("LED ON \r\n");	 		 /* 第三步：执行相应操作 */
+        }
+        else
+        {
+            printf("LED OFF \r\n");	 		 /* 第三步：执行相应操作 */
+        }
 	}
 }
 
 void my_gui(void)
 {
-    lv_obj_t  *btn  = lv_btn_create( lv_scr_act() );
+    switch1  = lv_switch_create( lv_scr_act() );
+    lv_obj_set_style_bg_color(switch1,lv_color_hex(0xdf5345), LV_STATE_CHECKED|LV_PART_INDICATOR);
 
-    lv_obj_set_size( btn, 100, 50 );								    /* 大小 */
-    lv_obj_set_align( btn, LV_ALIGN_CENTER );						    /* 对齐 */
-    lv_obj_set_style_bg_color( btn, lv_color_hex(0xffe1d4), LV_STATE_PRESSED );
+    lv_obj_add_state(switch1, LV_STATE_CHECKED|LV_STATE_DISABLED);		/* 添加状态：默认打开且不可修改 */
+    lv_obj_clear_state(switch1, LV_STATE_CHECKED | LV_STATE_DISABLED);	/* 清除开关的状态 */
 
-    lv_obj_add_flag( btn, LV_OBJ_FLAG_CHECKABLE );
-    lv_obj_add_event_cb( btn, my_event_cb, LV_EVENT_VALUE_CHANGED,NULL );	    /* 添加事件 */
-
+    lv_obj_add_event_cb(switch1,event_cb,LV_EVENT_VALUE_CHANGED,NULL);
 }
